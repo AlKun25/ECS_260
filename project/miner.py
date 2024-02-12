a8e750b409ff3e700273a8fd8bd81e36afcfbb16
Kunal <mundadakunal25@gmail.com>
2024-02-11 19:32:50 -0800

    Add project selection script

--- project/miner.py
+++ project/miner.py
@@ -1,43 +1,39 @@
 import os
-from github import Github, Repository, User
+from github import Auth, Github, Repository, Organization
+import pandas as pd
+from tqdm import tqdm
+from icecream import ic
 from dotenv import load_dotenv
+import warnings
 
+# System setup
+warnings.filterwarnings('ignore')
 load_dotenv()
 
-# Authentication is defined via github.Auth
-from github import Auth
-
-# using an access token
+# GitHub Auth
 auth = Auth.Token(os.getenv("GITHUB_PAT"))
 
-# First create a Github instance:
-
-def get_commit_count(repo: Repository.Repository):
-    return repo.get_commits().totalCount
-
-def get_commit_count(repo: Repository.Repository, file_path: str):
-    return repo.get_commits(path=file_path).totalCount
-
-def get_contributors(repo: Repository.Repository):
-    return repo.get_contributors()
-
-def get_all_files(repo: Repository.Repository):
-    return repo.get_dir_contents("./")
-
-def get_events_repo(repo: Repository.Repository):
-    return repo.get_events()
-
-def commit_time_diff(commit1, commit2):
-    pass
-
-# Public Web Github
+# GitHub object
 g = Github(auth=auth)
 
-repos = ["pytorch/pytorch"]
-# Then play with your Github objects:
-for repo in repos:
-    open_issues = repo.get_issues(state='open')
-    print(open_issues.totalCount)
+# Load list of all organizations
+orgs = pd.read_csv("./project/org_list.csv")
 
-# To close connections after use
-g.close()
+# print(orgs['link'][:])
+
+# Loop through each organization
+for idx, link in enumerate(orgs['link'][:]):
+    ic(idx, link)
+    org = g.get_organization(link.split(sep='/')[-1])
+    print(f"{idx}. {orgs['name'][idx]} organization loaded")
+    
+    # TODO: Get all repos of the organization
+    repos = org.get_repos()
+    ic(repos.totalCount)
+    
+    # TODO: Find all the repos above 100 stars
+    popularRepos = []
+    for repo in tqdm(repos):
+        if repo.stargazers_count > 100:
+            popularRepos.append(repo.name)
+    ic(len(popularRepos))
