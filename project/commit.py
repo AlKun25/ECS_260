7d8a821a7e5c629228d025e3c4d7abf926d051b0
dale0302 <ndale@ucdavis.edu>
2024-02-25 15:57:05 -0800

    added n_shared_commits and shared_dev_names

--- project/commit.py
+++ project/commit.py
@@ -13,7 +13,6 @@ import warnings
 
 from utils import *
 from constants import *
-from developer import developer_history
 
 # System setup
 warnings.filterwarnings("ignore")
@@ -31,6 +30,58 @@ auth = Auth.Token(os.getenv("GITHUB_PAT"))
 
 # GitHub object
 g = Github(auth=auth)
+
+def get_repo_org_commit(url):
+    if (url):
+        commit_repo_org_part = url.split(sep="repos/")[-1] 
+        # print(commit_repo_org_part)
+        org_repo_commit_list = commit_repo_org_part.split(sep="/")
+        org = org_repo_commit_list[0]
+        repo = org_repo_commit_list[1]
+        commit = org_repo_commit_list[-1]
+        # print(org, repo, commit)
+        return org, repo, commit
+    else:
+        return None, None, None
+
+def get_username(url):
+    username = url.split(sep="users/")[-1]
+    return username
+
+def developer_history(name: str, email: str, ref_org_repo: str, obs_start: datetime, obs_end: datetime):
+    # print(name, obs_start, obs_end, ref_org_repo, emails)
+    email_commits = []
+    shared = False
+    # Find commits made by that email, before Jan 1 2024
+    outside_repo_commits = 0
+    within_org_commits = 0
+    obs_end = obs_end + relativedelta(days=1)
+    commits = g.search_commits(query=f'author-email:{email} committer-date:<{obs_end.strftime("%Y-%m-%d")}', sort='committer-date', order='desc')
+    print(commits.totalCount)
+    if commits.totalCount>0:
+        for commit in commits:
+            # print(commit.author.html_url)
+            # user_name = get_username(commit.author) # to avoid individuals repo commits
+            # ic(commit.author)
+            # ic(commit.author.url)
+            if commit.author is not None:
+                user_name = get_username(commit.author.url)
+            else:
+                return "Non-existent users"
+            commit_org, commit_repo, commit_etag = get_repo_org_commit(url=commit.commit.url)
+            if(commit.commit.committer.date.date() < obs_start.date()):# terminate when outside OBS period
+                break
+            else:
+                if(commit_org != user_name):
+                    commit_date = commit.commit.committer.date
+                    email_commits.append([name, user_name, commit_repo, commit_org, commit_date.day, commit_date.month, commit_date.year, email, commit_etag])
+                    
+                    if(f"{commit_org}/{commit_repo}" != ref_org_repo): # ! : to check for shared developer
+                        shared = True
+                        outside_repo_commits += 1
+                        if(commit_org == ref_org_repo.split(sep="/")[0]):
+                            within_org_commits += 1
+    return outside_repo_commits
 
 # TODO: List of all selected repos
 repos = pd.read_csv(SELECT_REPOS_CSV)
@@ -53,16 +104,18 @@ for repo in repos["url"]:
         start_date = datetime.combine(start_date, datetime.min.time())
         finish_date = datetime.combine(finish_date, datetime.min.time())
         load_repo = Repository(
-            path_to_repo=repo_pth, since=start_date, to=finish_date, num_workers=8
+            path_to_repo=repo_pth, since=start_date, to=finish_date, num_workers=4
         )
         code_complexity = []
         commit_size = []
-
+        shared = 0
         contributor_to_email = dict()
         contributor_to_commits = dict()
         loa = []  # lines of anything
         modified_files = []
         count = 0
+        commits_by_shared = 0
+        shared_dev = []
         for commit in tqdm(load_repo.traverse_commits()):
             code_complexity.append(commit.dmm_unit_complexity)
             commit_size.append(commit.dmm_unit_size)
@@ -82,10 +135,14 @@ for repo in repos["url"]:
             modified_files.append(len(commit.modified_files))
             loa.append(commit.lines)
             count += 1
-        print(contributor_to_commits)
-        # for name in contributor_to_email.keys():
-            # developer_history(name=name, emails=list(contributor_to_email[name]), ref_org_repo=f"{org_name}/{repo_name}", obs_start=start_date, obs_end=finish_date)
-
+        # print(contributor_to_commits)
+        for name in contributor_to_email.keys():
+            for email in list(contributor_to_email[name]):
+                outside_commits = developer_history(name=name, email=email, ref_org_repo=f"{org_name}/{repo_name}", obs_start=start_date, obs_end=finish_date)
+                if (outside_commits != "Non-existent users" and outside_commits>0):
+                    shared += 1
+                    commits_by_shared += contributor_to_commits.get(name, 1)
+                    shared_dev.append(name)
         print("Total commits: ", count)
         logging.warning(f"Total commits in this month: {count}")
         monthly_row = []
@@ -99,9 +156,12 @@ for repo in repos["url"]:
                 get_metric_stats(pd.DataFrame(commit_size)),
                 get_metric_stats(pd.DataFrame(loa)),
                 get_metric_stats(pd.DataFrame(modified_files)),
-                len(contributor_to_email),
+                len(contributor_to_commits),
                 count,
-                contributor_to_commits
+                contributor_to_commits,
+                shared, 
+                commits_by_shared, 
+                shared_dev
             ]
         else:
             monthly_row = [
@@ -114,6 +174,9 @@ for repo in repos["url"]:
                 0,
                 0,
                 0,
+                '',
+                0,
+                0, 
                 ''
             ]
         month_df = pd.DataFrame(
@@ -128,7 +191,10 @@ for repo in repos["url"]:
                 "n_modified_files",
                 "n_contributors",
                 "n_commits",
-                "contributor_to_commits"
+                'contributors_to_commits',
+                "n_shared", 
+                "commits_by_shared",
+                "shared_dev"
             ],
         )
         logging.warning("Month added to commit activity CSV.")
@@ -140,3 +206,4 @@ for repo in repos["url"]:
     # logging.warning(f"Contributor count: {len(overall_repo_contributors)}")
     logging.warning(f"Actve months: {active_months}")
     delete_repo(repo_directory=repo_pth)
+
