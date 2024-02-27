47658b213e6b7bbf8693f5bfdb977fa69eb6dc3d
Kunal <mundadakunal25@gmail.com>
2024-02-27 07:48:20 -0800

    Add common logger function in  utils.py

--- project/utils.py
+++ project/utils.py
@@ -1,7 +1,10 @@
 import os
 import shutil
 import subprocess
+import logging
+from datetime import datetime
 import pandas as pd
+from constants import LOGS_PTH
 
 def add_to_csv(df: pd.DataFrame, csv_pth: str):
     if os.path.isfile(csv_pth):
@@ -36,3 +39,11 @@ def get_metric_stats(metric: pd.DataFrame):
     stats["Q1"], stats["avg"], stats["Q3"] = list(metric.quantile([0.25,0.5,0.75]).values)
     stats["Q1"], stats["avg"], stats["Q3"] = float(stats["Q1"]), float(stats["avg"]), float(stats["Q3"])
     return stats
+
+def get_logger(filename: str, log_level = logging.INFO):
+    logger = logging.basicConfig(
+        filename=f"{LOGS_PTH}/{filename.split(sep='/')[-1]}_{datetime.now()}.log",
+        level=log_level,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+    return logger
