c17df1188a45653ee38b896396ee16bb2e295745
Kunal <mundadakunal25@gmail.com>
2024-03-02 00:52:43 -0800

    Convert CSV paths to PARQUET, and add directories

--- project/constants.py
+++ project/constants.py
@@ -9,12 +9,12 @@ OBS_START_DATE = datetime.combine(date(2022, 1, 1), datetime.min.time())
 OBS_END_DATE = datetime.combine(date(2023, 12, 31), datetime.max.time())
 
 ORG_LIST_CSV = "/Users/kunalmundada/Documents/code/ECS_260/project/db/org_list.csv"
-SELECT_REPOS_CSV = "/Users/kunalmundada/Documents/code/ECS_260/project/db/selected_repos.csv"
+SELECT_REPOS_FILE = "/Users/kunalmundada/Documents/code/ECS_260/project/db/selected_repos.parquet"
 DEVELOPERS_CSV = "/Users/kunalmundada/Documents/code/ECS_260/project/db/developers.csv"
-DEVELOPER_ACTIVITY_CSV = "/Users/kunalmundada/Documents/code/ECS_260/project/db/developer_activity.csv"
-COMMIT_ACTIVITY_CSV = "/Users/kunalmundada/Documents/code/ECS_260/project/db/commit_activity.csv"
+DEVELOPER_ACTIVITY_DIR = "/Users/kunalmundada/Documents/code/ECS_260/project/db/devs"
+ORG_COMMITS_DIR = "/Users/kunalmundada/Documents/code/ECS_260/project/db/orgs"
 
 LOGS_PTH = "/Users/kunalmundada/Documents/code/ECS_260/project/logs"
 REPO_CLONE_DIR = "/Users/kunalmundada/Documents/code/ECS_260/project/repo_holder"
 
-GITHUB_OBJ = Github(auth=Auth.Token(os.getenv("GITHUB_PAT")))
+GITHUB_OBJ = Github(auth=Auth.Token(os.getenv("GITHUB_PAT"))) # type: ignore
